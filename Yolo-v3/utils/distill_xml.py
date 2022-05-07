f0f409cd352118ff0cc0cdab0216eb74d6b4af93
neongenesis <909818389@qq.com>
2022-05-08 02:47:44 +0800

    重命名

--- Yolo-v3/utils/distill_xml.py
+++ Yolo-v3/utils/distill_xml.py
@@ -1,42 +1,39 @@
-from xml.etree import ElementTree as ET
+import argparse
+import os
 from pathlib import Path
+from xml.etree import ElementTree as ET
 
 import numpy as np
-import argparse
 
 
 def argument():
     parser = argparse.ArgumentParser(description="pascalvoc convesion to yolo")
-    parser.add_argument("source", help="pascalvoc source savedir")
+    parser.add_argument("--source", help="pascalvoc source savedir", default="../data/Annotation")
     parser.add_argument("--target", "-o", default="../data", help="yolo target savedir")
-    args = parser.parse_args()
+    args = parser.parse_args(['--source', '../data/Annotation', '--target', '../data'])
     return args
 
 
-def convert(filename):
-    tree = ET.parse(filename)
+def convert(xmlpath):
+    tree = ET.parse(xmlpath)
     root = tree.getroot()
-    size = root.find("size")
-    width = int(size.find("width").text)
-    height = int(size.find("height").text)
-    box = list()
+    ground = list()
 
     for obj in root.findall("object"):  # 树的层次遍历，只在一级层次中查找标签为object的结点
         name = obj.find("name").text
         bndbox = obj.find("bndbox")
-        xyxy = [float(bndbox.find(s).text) for s in ["xmin", "ymin", "xmax", "ymax"]]
-        box.append((name, xyxy))
+        xyxy = [float(bndbox.find(s).text) for s in ("xmin", "ymin", "xmax", "ymax")]
+        xyxy.insert(0, name)
+        ground.append(tuple(xyxy))
 
-    if box:
+    if ground:
         filename = root.find("filename").text
-        dtype = np.dtype(
-            [("name", str, 40), ("xyxy", float, 4)]
-        )  # note:结构化数组，元素为序列时要指定长度，或是单独指定序列中的每个元素字段
-        annotation = np.array(box, dtype=dtype)
-        annotation["xyxy"][:, [0, 1]] /= width
-        annotation["xyxy"][:, [2, 3]] /= height
+        filepath = root.find("path").text
+
+        if not os.path.exists(filepath):
+            filepath = filepath.replace('MyYOLO', 'MyPapersCode/Yolo-v3')
         print(filename, "successful!")
-        return filename, annotation["name"], annotation["xyxy"]
+        return filepath, ground
     else:
         print("不存在真实框")
 
@@ -48,8 +45,9 @@ if __name__ == "__main__":
     outcome = list()
     for file in files:
         outcome.append(convert(file))
-    outcome = np.asarray(outcome, dtype=object)
+
     try:
+        outcome = np.array(outcome, dtype=np.object_)
         np.save(tar.joinpath("train_val"), outcome)
     except DeprecationWarning as e:
         print(e)
